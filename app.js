9aed2c15432ec962a8e90b2b7531d5b4144a4a91
tbarf <30032737+tbarf@users.noreply.github.com>
2017-08-16 19:49:50 -1000

    done

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 /*Create a for loop that will iterate through 100 numbers starting from 1 and do the following:
 
 if the number is a multiple of 3, it will console.log "fizz",
-if the number is a multiple of 5, it will console.log "buzz",  
+if the number is a multiple of 5, it will console.log "buzz",
 if the number is a multiple of 3 and 5, it will console.log "fizzBuzz"
 */
+
+
+
+for (var i = 1; i <= 100; i++) {
+  if (i % 5 === 0) {
+    console.log("buzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else (i % 5 === 0 && i % 3 === 0);
+  console.log("fizzBuzz");
+}
